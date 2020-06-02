e11dcf22a22a0bccc337498d5d887dccc6adf590
Karen Lei <karenwinnielei@gmail.com>
2020-06-01 19:42:18 -0700

    complete MVP

---
+++ src/components/TodoForm.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+class TodoForm extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      newTask: ''
+    }
+  }
+
+  handleChanges = e => {
+    this.setState({
+      newTask: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addTask(this.state.newTask);
+    this.setState({
+        newTask:''
+    })
+  }
+
+  render(){
+      return (
+          <div>
+          <form onSubmit={this.handleSubmit}>
+              <input
+                type = 'text'
+                name = 'task'
+                value = {this.state.newTask}
+                onChange = {this.handleChanges}
+              />
+              <button>Add Todo</button>
+          </form>
+          <button onClick = {this.props.clearCompleted}>
+                Clear Completed
+          </button>
+
+          </div>
+      )
+  }
+}
+
+export default TodoForm
